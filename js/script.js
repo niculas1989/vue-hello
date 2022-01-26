93e3cfca1f41adf15555a6fd4779194b56ba08e7
niculas1989 <nicolas.maranzano@libero.it>
2022-01-26 15:07:23 +0100

    added class in HTML + deleted link in IMG (JS)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const root = new Vue({
     el: '#root',
     data: {
         message: 'Hello Word!',
-        img: 'https://itakon.it/wp-content/uploads/2020/09/JohnWickChapter3Promo.jpg',
+        img: '',
     }
 })
