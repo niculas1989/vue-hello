f22c87dea41cbc01b018dc2c18e2f168dce0b622
niculas1989 <nicolas.maranzano@libero.it>
2022-01-26 15:03:02 +0100

    added IMG - always with Vue

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const root = new Vue({
     el: '#root',
     data: {
         message: 'Hello Word!',
-        img: 'URL',
+        img: 'https://itakon.it/wp-content/uploads/2020/09/JohnWickChapter3Promo.jpg',
     }
 })
